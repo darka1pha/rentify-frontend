c22d7a7021800b838379975fb92ccb3dea568579
DarkAlpha <abolfazl.omrani1999@gmail.com>
2025-01-24 23:55:34 +0330

    colors added

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,11 +1,34 @@
-'use client';
-import { createTheme } from '@mui/material/styles';
+"use client";
+import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  direction: 'rtl',
-  typography: {
-    fontFamily: 'var(--font-roboto)',
-  },
+	direction: "rtl",
+	palette: {
+		primary: {
+			main: "#0D6EFD",
+			light: "#4791ff",
+			dark: "#004ec2",
+			contrastText: "#ffffff",
+		},
+		error: {
+			main: "#c30000",
+			light: "#ed2e2e",
+			dark: "#9a0000",
+			contrastText: "#ffffff",
+		},
+		success: {
+			main: "#00966d",
+			light: "#00b38b",
+			dark: "#00624a",
+			contrastText: "#ffffff",
+		},
+		warning: {
+			main: "#a9791c",
+			light: "#f4b740",
+			dark: "#7a5a12",
+			contrastText: "#000000",
+		},
+	},
 });
 
 export default theme;
