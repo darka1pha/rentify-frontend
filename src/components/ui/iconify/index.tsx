ea18458edfe60f511f256df9aa542485d9402671
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2025-03-09 08:58:31 +0330

    iconify component added

---
+++ src/components/ui/iconify/index.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { forwardRef } from 'react';
+import { Icon } from '@iconify/react';
+
+import Box, { BoxProps } from '@mui/material/Box';
+import NoSsr from '@mui/material/NoSsr';
+import { IconProps } from '@mui/material';
+
+const iconifyClasses = {
+  root: 'mnl__icon__root',
+};
+
+export type IconifyProps = BoxProps & IconProps;
+
+const Iconify = forwardRef<SVGElement, IconifyProps>(
+  ({ className, width = 20, sx, ...other }, ref) => {
+    const baseStyles = {
+      width,
+      height: width,
+      flexShrink: 0,
+      display: 'inline-flex',
+    };
+
+    const renderFallback = (
+      <Box
+        component='span'
+        className={iconifyClasses.root.concat(className ? ` ${className}` : '')}
+        sx={{ ...baseStyles, ...sx }}
+      />
+    );
+
+    return (
+      <NoSsr fallback={renderFallback}>
+        <Box
+          ref={ref}
+          component={Icon}
+          className={iconifyClasses.root.concat(
+            className ? ` ${className}` : ''
+          )}
+          sx={{ ...baseStyles, ...sx }}
+          {...other}
+        />
+      </NoSsr>
+    );
+  }
+);
+
+export default Iconify;
