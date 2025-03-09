7281a9b5606267f38b1680711f5ff7a5be2f67f3
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2025-03-09 09:00:37 +0330

    unused tags removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-'use client';
 import { ThemeSwitch } from '@/components/ui';
-import { Button, Typography, useColorScheme } from '@mui/material';
-import { useTranslations } from 'next-intl';
 
 export default function Home() {
-  const { mode, setMode } = useColorScheme();
-  const t = useTranslations('home');
-  console.log(mode);
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <Typography>تست</Typography>
       <ThemeSwitch />
     </div>
   );
